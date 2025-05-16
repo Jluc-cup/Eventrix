14042fcaf51ce33328abba60c99e66a56efd3aa7
Alisienok Nikita <jlucenokgogo@gmail.com>
2025-05-16 23:01:33 +0300

    start

---
+++ src/main/java/com/eventrix/EventrixApplication.java
@@ -0,0 +1,13 @@
+package com.eventrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventrix/EventrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
